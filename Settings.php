f178e96452546fd582d8b0e2b8981b50714f79fc
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 14:58:55 -0500

    Create Settings.php

---
+++ Settings.php
@@ -0,0 +1,14 @@
+<?php
+$settings['database'] = [
+    'driver' => 'mysql',
+    'database' => 'your_database',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'host' => 'localhost',
+];
+
+$settings['site_name'] = 'My Site';
+$settings['cache'] = [
+    'default' => 'cache.backend.database',
+];
+?>
