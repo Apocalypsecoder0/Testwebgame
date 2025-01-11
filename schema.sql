7348e34fab7f9fa2e1e88f04a0c5bcfc021d5179
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 17:16:13 -0500

    Create  schema.sql

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender VARCHAR(50) NOT NULL,
+    recipient VARCHAR(50) NOT NULL,
+    message TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
