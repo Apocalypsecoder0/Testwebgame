6a398f085b21fa59fc5f1b7d4c2e24d25cdbd716
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 20:08:26 -0500

    Update Index.php
    Signed-off-by: Apocalypsecoder0 <Stephend8846@gmail.com>

--- Chat/Index.php
+++ Chat/Index.php
@@ -1,1 +1,16 @@
+<?php
 
+// Suppress errors:
+error_reporting(E_ALL);
+
+// Path to the chat directory:
+define('AJAX_CHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
+
+// Include custom libraries and initialization code:
+require(AJAX_CHAT_PATH.'lib/custom.php');
+
+// Include Class libraries:
+require(AJAX_CHAT_PATH.'lib/classes.php');
+
+// Initialize the chat:
+$ajaxChat = new CustomAJAXChat();
