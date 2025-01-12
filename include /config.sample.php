0418820d78b50d5e4e7a749d962d34a2f0dc96d9
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 20:42:45 -0500

    Create config.sample.php
    Signed-off-by: Apocalypsecoder0 <Stephend8846@gmail.com>

---
+++ include /config.sample.php
@@ -0,0 +1,19 @@
+
+<?php
+
+//### Database Configuration ###//
+
+$database = [
+    'host'          => '%s', // Database host
+    'port'          => '%s', // Database port
+    'user'          => '%s', // Database username
+    'userpw'       => '%s', // Database password
+    'databasename'  => '%s', // Database name
+    'tableprefix'   => '%s'  // Table prefix for the database
+];
+
+// Salt for password hashing (22 characters from the alphabet "./0-9A-Za-z")
+$salt = '%s'; 
+
+//### Do not change beyond here ###//
+?>
