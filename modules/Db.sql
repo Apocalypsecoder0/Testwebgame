75ddbe508ab5ffb890ade86188747c5e265bdb06
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-12 00:21:12 -0500

    Update Db.sql
    Signed-off-by: Apocalypsecoder0 <Stephend8846@gmail.com>

--- modules/Db.sql
+++ modules/Db.sql
@@ -19,3 +19,61 @@ CREATE TABLE game_actions (
     target_id INT NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+MySQL Table Structures
+users
+CREATE TABLE `users` (
+    `uid` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(255),
+    `password` VARCHAR(255),
+    `onHand` DECIMAL(10, 2)
+);
+weapons
+CREATE TABLE `weapons` (
+    `wid` INT PRIMARY KEY AUTO_INCREMENT,
+    `uid` INT,
+    `strength` INT,
+    `quanity` INT,
+    FOREIGN KEY (uid) REFERENCES users(uid)
+);
+armory
+CREATE TABLE `armory` (
+    `wid` INT PRIMARY KEY,
+    `weaponName` VARCHAR(255),
+    `weaponPower` INT,
+    `cash_cost` DECIMAL(10, 2),
+    `isDefense` BOOLEAN
+);
+bank
+CREATE TABLE `bank` (
+    `uid` INT PRIMARY KEY,
+    `onHand` DECIMAL(10, 2),
+    FOREIGN KEY (uid) REFERENCES users(uid)
+);
+Example Queries
+Repair Weapon
+UPDATE `weapons` 
+SET `strength` = (SELECT weaponPower FROM armory WHERE wid = $id) 
+WHERE uid = {$_SESSION['userid']} AND wid = $id;
+Sell Weapons
+SELECT armory.wid, armory.weaponName, weapons.strength, armory.weaponPower, 
+       armory.cash_cost, armory.isDefense, weapons.quanity
+FROM `armory`, `weapons`, `userdata`
+WHERE weapons.uid = {$_SESSION['userid']} AND weapons.wid = $wid
+  AND armory.wid = weapons.wid
+  AND userdata.uid = weapons.uid
+  AND armory.rid = userdata.rid
+LIMIT 1000;
+
+UPDATE `weapons` 
+SET `quanity` = `quanity` - '$id' 
+WHERE `uid` = {$_SESSION['userid']} AND `wid` = $wid LIMIT 1;
+
+UPDATE `bank` 
+SET `onHand` = `onHand` + '$costtosell' 
+WHERE `uid` = {$_SESSION['userid']} LIMIT 1;
+
+DELETE FROM `weapons` 
+WHERE `uid` = {$_SESSION['userid']} AND `wid` = $wid AND `quanity` = '0' LIMIT 1;
+Buy Weapons
+INSERT INTO `weapons` (uid, strength, quanity) 
+VALUES ({$_SESSION['userid']}, $strength, $quantity);
