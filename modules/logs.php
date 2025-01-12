32aca34f05ba7217e86f5f1097fc9b4d38dd0697
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-12 00:44:01 -0500

    Create logs.php
    Signed-off-by: Apocalypsecoder0 <Stephend8846@gmail.com>

---
+++ modules/logs.php
@@ -0,0 +1,29 @@
+<?php 
+include("../config.php");
+
+$pagegen = new page_gen();
+$pagegen->round_to = 4;
+$pagegen->start();
+
+$s = new Game();
+if (!$s->loggedIn || !isset($_GET['time'])) {
+    header("Location: https://realmbattles.org/SGWnew/index.php?");
+    exit();
+}
+
+$s->updatePower($_SESSION['userid']);
+?>
+
+<center>
+    <a href="javascript:void(0)" onclick="sendData('logs','get','id','attack');return false">Attack</a> | 
+    <a href="javascript:void(0)" onclick="sendData('logs','get','id','raid');return false">Raid</a> | 
+    <a href="javascript:void(0)" onclick="sendData('logs','get','id','spy');return false">Spy</a> | 
+    <a href="javascript:void(0)" onclick="sendData('logs','get','id','sab');return false">Sabotage</a>
+</center>
+
+<?php 
+$s->actionLog($_GET['atype']);
+echo "Query Count: " . $s->queryCount . "<br>";
+$pagegen->stop();
+echo 'Page generation time: ' . $pagegen->gen();
+?>
